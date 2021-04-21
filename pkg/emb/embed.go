a39a209bdfbb79480dd6b79cfc9699db7ea6bf89
bingoohuang <bingoo.huang@gmail.com>
2021-04-21 12:36:23 +0800

    embed Asset

---
+++ pkg/emb/embed.go
@@ -0,0 +1,68 @@
+package emb
+
+import (
+	"bytes"
+	"compress/gzip"
+	"crypto/md5"
+	"embed"
+	"encoding/hex"
+	"io"
+	"io/fs"
+	"mime"
+	"net/http"
+	"path/filepath"
+)
+
+// from https://github.com/pyros2097/go-embed
+
+// Asset Gets the file from embed.FS if debug otherwise gets it from the stored
+// data returns the data, the md5 hash of its content and its content type and
+// and error if it is not found.
+func Asset(f embed.FS, name string) (data []byte, hash, contentType string, err error) {
+	var fn fs.File
+	fn, err = f.Open(name)
+	if err != nil {
+		return nil, "", "", err
+
+	}
+	data, err = io.ReadAll(fn)
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	if len(data) > 0 {
+		var b bytes.Buffer
+		w := gzip.NewWriter(&b)
+		_, _ = w.Write(data)
+		_ = w.Close()
+		data = b.Bytes()
+	}
+
+	sum := md5.Sum(data)
+	contentType = mime.TypeByExtension(filepath.Ext(name))
+	return data, hex.EncodeToString(sum[:]), contentType, nil
+}
+
+func FileHandler(f embed.FS, name string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) { ServeFile(f, name, w, r) }
+}
+
+func ServeFile(f embed.FS, name string, w http.ResponseWriter, r *http.Request) {
+	data, hash, contentType, err := Asset(f, name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.Header.Get("If-None-Match") == hash {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Add("Cache-Control", "public, max-age=31536000")
+	w.Header().Add("ETag", hash)
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(data)
+}
