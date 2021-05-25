9e7987e1dac30f772802bbacfd06da1edd28078a
bingoohuang <bingoo.huang@gmail.com>
2021-05-25 08:35:24 +0800

    RegisterSignals

---
+++ pkg/ctx/signal_test.go
@@ -0,0 +1,25 @@
+package ctx
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestRegisterSignals(t *testing.T) {
+	c := RegisterSignals(nil)
+	proc, _ := os.FindProcess(os.Getpid())
+	if err := proc.Signal(os.Interrupt); err != nil {
+		t.Failed()
+	}
+
+	done := false
+	select {
+	case <-c.Done():
+		done = true
+	case <-time.After(1 * time.Millisecond):
+	}
+
+	assert.True(t, done)
+}
