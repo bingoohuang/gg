bbf4832051808aab27038311aeee584b3a697c87
bingoohuang <bingoo.huang@gmail.com>
2021-11-16 09:34:15 +0800

    gg-img arg refactor

--- cmd/gg-img/main.go
+++ cmd/gg-img/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	flag "github.com/bingoohuang/gg/pkg/fla9"
+	"github.com/bingoohuang/gg/pkg/fla9"
 	"github.com/bingoohuang/gg/pkg/man"
 	"github.com/bingoohuang/gg/pkg/randx"
 )
 
 func main() {
-	w := flag.Int("w", 640, "picture width")
-	h := flag.Int("h", 320, "picture height")
-	fixedSizeStr := flag.String("s", "", "fixed size(eg. 44kB, 17MB)")
-	many := flag.Int("i", 1, "how many pictures to create")
-	picfmt := flag.String("f", "png", "picture format(png/jpg)")
-	fastMode := flag.Bool("fast", false, "fast mode")
-	flag.Parse()
+	w := fla9.Int("width,w", 640, "picture width")
+	h := fla9.Int("height,h", 320, "picture height")
+	fixedSizeStr := fla9.String("size,s", "", "fixed size(eg. 44kB, 17MB)")
+	many := fla9.Int("n", 1, "how many pictures to create")
+	picfmt := fla9.String("format,f", "png", "picture format(png/jpg)")
+	fastMode := fla9.Bool("fast", false, "fast mode")
+	fla9.Parse()
 
 	fixedSize := uint64(0)
 	if *fixedSizeStr != "" {
