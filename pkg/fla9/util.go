bc7d5ef8c11f20287eb4c1b2ded84c06f7fb3017
bingoohuang <bingoo.huang@gmail.com>
2021-11-29 14:46:25 +0800

    fla9.ParseFileArg also return filename

--- pkg/fla9/util.go
+++ pkg/fla9/util.go
@@ -8,16 +8,16 @@ import (
 
 // ParseFileArg parse an argument which represents a string content,
 // or @file to represents the file's content.
-func ParseFileArg(arg string) []byte {
+func ParseFileArg(arg string) (file string, data []byte) {
 	if strings.HasPrefix(arg, "@") {
 		f := (arg)[1:]
 		if v, err := ioutil.ReadFile(f); err != nil {
 			log.Fatalf("failed to read file %s, error: %v", f, err)
-			return nil
+			return f, nil
 		} else {
-			return v
+			return f, v
 		}
 	}
 
-	return []byte(arg)
+	return arg, []byte(arg)
 }
