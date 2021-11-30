863a22b48d2ec413b247ba78f691db0ff740d983
bingoo <bingoo.huang@gmail.com>
2021-11-30 23:14:26 +0800

    fla9.ExitOnError

--- pkg/fla9/fla9.go
+++ pkg/fla9/fla9.go
@@ -1186,7 +1186,7 @@ func Parsed() bool {
 // CommandLine is the default set of command-line flags, parsed from os.Args.
 // The top-level functions such as BoolVar, Arg, and so on are wrappers for the
 // methods of CommandLine.
-var CommandLine = NewFlagSet(os.Args[0], ContinueOnError)
+var CommandLine = NewFlagSet(os.Args[0], ExitOnError)
 
 func init() {
 	// Override generic FlagSet default Usage with call to global Usage.
