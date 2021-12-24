611c8ba5af8d0fa4f238d9c841167bdfe4a8f8cc
bingoohuang <bingoo.huang@gmail.com>
2021-12-24 16:27:05 +0800

    add emb.AssetString emb.AssetBytes

--- pkg/emb/embed.go
+++ pkg/emb/embed.go
@@ -10,14 +10,28 @@ import (
 	"mime"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 // from https://github.com/pyros2097/go-embed
+
+func AssetString(f fs.FS, name string, useGzip bool) string {
+	return string(AssetBytes(f, name, useGzip))
+}
+
+func AssetBytes(f fs.FS, name string, useGzip bool) (data []byte) {
+	data, _, _, _ = Asset(f, name, useGzip)
+	return data
+}
 
 // Asset Gets the file from embed.FS if debug otherwise gets it from the stored
 // data returns the data, the md5 hash of its content and its content type
 // and error if it is not found.
 func Asset(f fs.FS, name string, useGzip bool) (data []byte, hash, contentType string, err error) {
+	if strings.HasPrefix(name, "/") {
+		name = name[1:]
+	}
+
 	var fn fs.File
 	fn, err = f.Open(name)
 	if err != nil {
