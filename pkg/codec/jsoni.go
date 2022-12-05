a914006878fcadc5ce533ae5707a8f2f2511d12a
bingoohuang <bingoo.huang@gmail.com>
2022-12-05 10:53:33 +0800

    codec.ReadJsonFile/WriteJsonFile

--- pkg/codec/jsoni.go
+++ pkg/codec/jsoni.go
@@ -2,8 +2,10 @@ package codec
 
 import (
 	"context"
+	"fmt"
 	"github.com/bingoohuang/gg/pkg/jsoni"
 	"io"
+	"os"
 )
 
 func Json(v interface{}) []byte {
@@ -25,4 +27,32 @@ func EncodeJson(w io.Writer, v interface{}) error {
 
 func DecodeJson(r io.Reader, obj interface{}) error {
 	return jsoni.NewDecoder(r).Decode(context.Background(), obj)
+}
+
+func ReadJsonFile(obj interface{}, file string) error {
+	// 读取配置文件
+	configFile, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("read config file %s failed: %w", file, err)
+	}
+
+	// 反序列化到结构体中
+	if err := jsoni.Unmarshal(configFile, obj); err != nil {
+		return fmt.Errorf("unmarshal file data failed: %w", err)
+	}
+
+	return nil
+}
+
+func WriteJsonFile(obj interface{}, indent, file string) error {
+	data, err := jsoni.MarshalIndent(obj, "", indent)
+	if err != nil {
+		return fmt.Errorf("MarshalIndent failed: %w", err)
+	}
+
+	if err := os.WriteFile(file, data, os.ModePerm); err != nil {
+		return fmt.Errorf("writing config file %s failed: %w", file, err)
+	}
+
+	return nil
 }
